14da66fa2f0e79444d5139a2289fb20720ad12b1
Bo Bayles <bbayles@gmail.com>
2025-12-29 10:15:40 -0600

    Add static codes for LMA 2003

--- assets/brython/lma_manager_2003.py
+++ assets/brython/lma_manager_2003.py
@@ -1,25 +1,33 @@
+BASE_CODES = {
+    "Sell Out": "LMA2003MA",
+    "Anytime, Any Place": "LMA2003MB",
+    "That's All Folks": "LMA2003MC",
+    "Loadza Money": "LMA2003A",
+    "Low Gravity Ball": "LMA2003B",
+}
 ALL_EFFECTS = [
-    'Too Much Coffee',
-    'Rainbow Trails',
-    'Win, Win, Win',
-    'Pyscho Team',
-    'Selective Sight',
-    'Medicine Man',
-    'Instant Stadium',
-    'Dream Team',
-    'Super Team',
-    'Sit Back And Relax',
-    'Winter Sport',
-    'You Are My Sunshine',
-    'Rain God',
-    'Forever Young',
-    'Super Coach',
+    "Too Much Coffee",
+    "Rainbow Trails",
+    "Win, Win, Win",
+    "Pyscho Team",
+    "Selective Sight",
+    "Medicine Man",
+    "Instant Stadium",
+    "Dream Team",
+    "Super Team",
+    "Sit Back And Relax",
+    "Winter Sport",
+    "You Are My Sunshine",
+    "Rain God",
+    "Forever Young",
+    "Super Coach",
     "Don't Worry, Be Happy",
-    'Teamwork',
-    'Pinky and Perky',
-    'Lucifer',
-    'Go Now',
+    "Teamwork",
+    "Pinky and Perky",
+    "Lucifer",
+    "Go Now",
 ]
+
 
 def get_digits(val):
     low = val % 26
@@ -34,11 +42,11 @@ def generate_code(unique_id, index):
     exp_2 = pow(0x39, d ^ index, 0x44A5)
     c_0, c_1, c_2, c_3, c_4, c_5 = get_digits(exp_1) + get_digits(exp_2)
 
-    return f'LMA3{c_0}{c_2}{c_1}{c_4}{c_5}{c_3}'
+    return f"LMA3{c_0}{c_2}{c_1}{c_4}{c_5}{c_3}"
 
 
 def get_all_codes(unique_id):
-    ret = {}
+    ret = BASE_CODES.copy()
     for i, effect in enumerate(ALL_EFFECTS):
         ret[effect] = generate_code(unique_id, i)
 
@@ -57,4 +65,4 @@ if __name__ == "__main__":
 
     all_codes = get_all_codes(parsed_args.unqiue_id)
     for effect, code in all_codes.items():
-        print(f'{effect}: {code}')
+        print(f"{effect}: {code}")
